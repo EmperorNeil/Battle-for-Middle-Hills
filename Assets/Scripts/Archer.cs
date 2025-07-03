7121a8d833e5f8e0b5094f6014b048d4e87050b9
YannickMG <yannickmg@gmail.com>
2025-07-03 17:50:51 -0400

    Added Archer Prefab

---
+++ Assets/Scripts/Archer.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class Archer : MonoBehaviour
+{
+
+    [SerializeField]
+    private Team team;
+    public Team Team { set => team = value; }
+
+    [Header("Sprites")]
+
+    [SerializeField]
+    private ArcherSpriteSet blueSprites;
+    [SerializeField]
+    private ArcherSpriteSet redSprites;
+
+
+
+    void OnValidate()
+    {
+        switch (team)
+        {
+            case Team.Blue:
+                transform.localScale = new Vector3(-1, 1, 1);
+                setSprites(blueSprites);
+                break;
+            case Team.Red:
+                transform.localScale = new Vector3(1, 1, 1);
+                setSprites(redSprites);
+                break;
+        }
+    }
+
+    private void setSprites(ArcherSpriteSet sprites)
+    {
+        Transform body = transform.Find("Body");
+        body.gameObject.GetComponent<SpriteRenderer>().sprite = sprites.body;
+        body.Find("Back arm").gameObject.GetComponent<SpriteRenderer>().sprite = sprites.backArm;
+        body.Find("Front arm").gameObject.GetComponent<SpriteRenderer>().sprite = sprites.frontArm;
+    }
+
+}
+
+[System.Serializable]
+public class ArcherSpriteSet
+{
+    [SerializeField]
+    public Sprite body;
+    [SerializeField]
+    public Sprite frontArm;
+    [SerializeField]
+    public Sprite backArm;
+}
