88cf6f469dcfeed5a8e02c521043b74c63904776
YannickMG <yannickmg@gmail.com>
2025-07-03 16:49:20 -0400

    Added army movement

--- Assets/Scripts/BattleForMiddleHillsController.cs
+++ Assets/Scripts/BattleForMiddleHillsController.cs
@@ -2,15 +2,31 @@ using UnityEngine;
 
 public class BattleForMiddleHills : MonoBehaviour
 {
+
+    [SerializeField]
+    private GameObject redArmy;
+    [SerializeField]
+    private GameObject blueArmy;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        moveArmy(blueArmy.transform, Vector3.right);
+        moveArmy(redArmy.transform, Vector3.left);
     }
+
+    private void moveArmy(Transform armyTransform, Vector3 direction)
+    {
+        foreach (Transform unit in armyTransform)
+        {
+            unit.transform.Translate(direction * Time.deltaTime);
+        }
+    }
+
 }
